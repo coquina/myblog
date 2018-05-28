c888b5a78c0d37981efdeb48341168df8c554474
coquina <tommy1224280@gmail.com>
2018-05-28 13:42:14 +0800

    new page about ,article

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ from main import views
 app_name = 'main'
 urlpatterns = [
     path('', views.main, name='main'),
+    path('about/' , views.about , name='about'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
-def main(requeset):
+def main(request):
     '''
-    SHOW'Hello world! in the main page
+    Render the main page
     :param requeset:
     :return:
     '''
-    return HttpResponse('Hello world!')
+    context={'like': 'Django 很棒'}
+    return render(request, 'main/main.html', context)
+def about(request):
+    '''
+    Render the about page
+    :param request:
+    :return:
+    '''
+    return render(request, 'main/about.html' )
