70af80444aefc668677f7cc028099d987d7d8e18
coquina <tommy1224280@gmail.com>
2018-05-28 19:14:06 +0800

    new page about ,article

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
 
 def main(request):
@@ -8,8 +8,26 @@ def main(request):
     :param requeset:
     :return:
     '''
-    context={'like': 'Django 很棒'}
-    return render(request, 'main/main.html', context)
+    like = 'Django好棒'
+    time = datetime.now()
+    return render(request, 'main/main.html', locals())
+# def time(request):
+#     '''
+#
+#     :param request:
+#     :return:
+#     '''
+#     noontime = datetime.now().strptime('12:0:0')
+#     now = datetime.now().strftime('%H:%M:%S')
+#     if now > str(noontime):
+#         show = '午安'
+#         return render(request, 'main/main.html', locals())
+#     else:
+#         show1 = '早安'
+#         return render(request, 'main/main.html', locals())
+
+
+
 def about(request):
     '''
     Render the about page
---
+++ populate/local.py
@@ -0,0 +1,7 @@
+from populate import admin, users, article
+
+
+
+admin.populate()
+users.populate()
+article.populate()
