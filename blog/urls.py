349655b43d8a4ee3c8ae6d1c35b00434c2a7bee5
coquina <tommy1224280@gmail.com>
2018-06-17 15:06:00 +0800

    css login page

--- blog/urls.py
+++ blog/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from main import views
+from article import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main', include('main.urls', namespace='main')),
     path('article', include('article.urls', namespace='article')),
     path('account', include('account.urls',namespace='account')),
-    re_path('.*', views.main),
+    re_path('.*', views.article),
 ]
